a890c54560924741a15d928144693af86ef0441b
Aryan Deora <adeora@iu.edu>
2023-06-17 01:40:08 -0400

    Add resource

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,69 +1,91 @@
-import { Show, createMemo, createSignal } from "solid-js";
+import {
+  Show,
+  createMemo,
+  createSignal,
+  type JSX,
+  createResource,
+  Suspense,
+  ErrorBoundary,
+} from "solid-js";
 
 const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-export default function App() {
-  const [cityName, setCityName] = createSignal('');
-  const [submittedCityName, setSubittedCityName] = createSignal('');
-  const [tempKelvin, setTempKelvin] = createSignal<number>();
-  const [error, setError] = createSignal(false);
-  const [tempUnit, setTempUnit] = createSignal('Celcius')
+type SubmitDataHandler = JSX.EventHandlerUnion<
+  HTMLFormElement,
+  Event & {
+    submitter: HTMLElement;
+  }
+>;
 
-  const tempCelsius = createMemo(() => {
-    const kelvinVal = tempKelvin();
-    return kelvinVal ? Math.round(kelvinVal - 273.15) : undefined;
+export default function App() {
+  const [cityName, setCityName] = createSignal<string | undefined>();
+  const [tempUnit, setTempUnit] = createSignal("Celcius");
+
+  const [temperature] = createResource(cityName, async (city) => {
+    const result = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    );
+    const weather = await result.json();
+    if (weather.cod !== 200) {
+      throw new Error(weather.message);
+    }
+    return {
+      kelvin: weather.main.temp as number,
+      celcius: Math.round(weather.main.temp - 273.15),
+    };
   });
 
-  const handleSubmit = async (e: SubmitEvent) => {
+  const handleSubmit: SubmitDataHandler = async (e) => {
     e.preventDefault();
-
-    if (!cityName()) return;
-
-    const result = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName()}&appid=${apiKey}`
-    );
-
-    const weather = await result.json();
-
-    setError(!weather?.main?.temp);
-    setTempKelvin(weather?.main?.temp);
-    setSubittedCityName(weather?.name);
+    const form = e.currentTarget;
+    const formElements = form.elements as typeof form.elements & {
+      cityName: HTMLInputElement;
+    };
+    const cityName = formElements.cityName.value;
+    if (!cityName) return;
+    setCityName(cityName);
   };
 
   return (
     <>
       <h2>Weather</h2>
       <form onSubmit={handleSubmit}>
-        <input value={cityName()} placeholder="City name" onInput={(e) => {
-          setCityName(e.target.value);
-        }} />
-        <button type="submit">
-          Check Weather
-        </button>
+        <input placeholder="City name" name="cityName" />
+        <button type="submit">Check Weather</button>
       </form>
-      <Show when={error()}>
-        <p>
-          <strong>An error occured</strong>
-        </p>
-      </Show>
-      <Show when={submittedCityName()}>
-        <p>
-          The current weather in <strong>{submittedCityName()}</strong> is:{' '}
 
-          <Show when={tempUnit() === 'Celcius'} fallback={tempKelvin()}>
-            {tempCelsius()}
+      <ErrorBoundary fallback={<div>Something Went Wrong</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Show when={temperature()}>
+            <p>
+              The current weather in <strong>{cityName()}</strong> is:{" "}
+              <Show
+                when={tempUnit() === "Celcius"}
+                fallback={temperature()?.kelvin}
+              >
+                {temperature()?.celcius}
+              </Show>
+            </p>
+            <p>
+              <button
+                onClick={() => {
+                  setTempUnit("Celcius");
+                }}
+              >
+                Celcius
+              </button>
+
+              <button
+                onClick={() => {
+                  setTempUnit("Kelvin");
+                }}
+              >
+                Kelvin
+              </button>
+            </p>
           </Show>
-        </p>
-        <p>
-
-          <button onClick={() => {
-            setTempUnit('Celcius');
-          }}>Celcius</button>
-
-          <button onClick={() => {
-            setTempUnit('Kelvin');
-          }}>Kelvin</button></p>
-      </Show>
+        </Suspense>
+      </ErrorBoundary>
     </>
-  )
+  );
 }
